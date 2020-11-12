fb9fe4bfa5a6d620a7451d0f9d1ef1416d68f757
Msy728 <755109626@qq.com>
2020-11-12 12:59:19 +0800

    two add

---
+++ app/src/main/java/com/example/work/fb/WnActivity.java
@@ -0,0 +1,16 @@
+package com.example.work.fb;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.work.R;
+
+public class WnActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wn);
+    }
+}
